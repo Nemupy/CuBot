f478ace3edd04a998c541aeaec28ae6af39ea1b6
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-05-01 09:02:18 +0900

    エラーメッセージ修正

--- discordbot.py
+++ discordbot.py
@@ -20,8 +20,7 @@ async def on_ready():
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    embed = discord.Embed(title="DETAIL-embed", description="Embedパネルを作成します。", colour=0x3498db)
-    embed.add_field(name="エラー", value="予期せぬエラーが発生しました。\nこのエラーが多発する場合は公式サーバーまでお問い合わせください。\n```error_msg```", inline=True)
+    embed = discord.Embed(title="エラー", description="予期せぬエラーが発生しました。\nこのエラーが多発する場合は公式サーバーまでお問い合わせください。\n```"+error_msg+"```", colour=0x3498db)
     await ctx.reply(embed=embed)
 
 @bot.event
