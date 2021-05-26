9052fbc32350acb09f8a3f87309409ab3d5270cb
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-05-26 20:28:46 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,7 +13,7 @@ bot = commands.Bot(command_prefix =["Cu!","cu!"], help_command = None, intents =
 
 @bot.event
 async def on_ready():
-    a = 847070931183861780
+    a = guild.get_channel(847070931183861780)
     await a.channel.send(f"<@798439010594717737> ログインしました") 
     servers = len(bot.guilds)
     members = 0
