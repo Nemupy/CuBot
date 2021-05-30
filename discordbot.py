aa73a10c65123332d4f791bf134f36b8fb229a64
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-05-30 17:51:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -544,7 +544,7 @@ async def pages(ctx):
     embed = discord.Embed(title="コマンドリスト-バラエティ",description="使用可能なコマンド一覧です♪", colour=0x3498db)
     embed.add_field(name="フィールドの名前",value="フィールドの値")
     cur_page = 1
-    message = await ctx.send(embed=embed+cur_page-1)
+    message = await ctx.send(embed=embed+int(cur_page-1))
     await message.add_reaction("◀️")
     await message.add_reaction("▶️")
     def check(reaction, user):
@@ -554,11 +554,11 @@ async def pages(ctx):
             reaction, user = await bot.wait_for("reaction_add", timeout=60, check=check)
             if str(reaction.emoji) == "▶️" and cur_page != pages:
                 cur_page += 1
-                await message.edit(embed=embed+cur_page-1)
+                await message.edit(embed=embed+int(cur_page-1))
                 await message.remove_reaction(reaction, user)
             elif str(reaction.emoji) == "◀️" and cur_page > 1:
                 cur_page -= 1
-                await message.edit(embed=embed+cur_page-1)
+                await message.edit(embed=embed+int(cur_page-1))
                 await message.remove_reaction(reaction, user)
             else:
                 await message.remove_reaction(reaction, user)
