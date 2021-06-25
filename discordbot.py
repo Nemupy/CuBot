76c41a2f14f052c2ed4037303cccaa39afe5965b
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-06-25 17:55:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -629,23 +629,23 @@ async def sinfo(ctx):
     await ctx.send(embed=embed)
 
 @bot.command()
-async def uinfo627(ctx, member : discord.Member):
+async def uinfo627(ctx, member : discord.Member = ctx.author):
     async with ctx.typing():
         await asyncio.sleep(0)
     date_format = "%a, %d %b %Y %I:%M %p"
-    embed = discord.Embed(color=0xdfa3ff, description=user.mention)
-    embed.set_author(name=str(user), icon_url=user.avatar_url)
-    embed.set_thumbnail(url=user.avatar_url)
-    embed.add_field(name="Joined", value=user.joined_at.strftime(date_format))
+    embed = discord.Embed(color=0xdfa3ff, description=member.mention)
+    embed.set_author(name=str(member), icon_url=member.avatar_url)
+    embed.set_thumbnail(url=member.avatar_url)
+    embed.add_field(name="Joined", value=member.joined_at.strftime(date_format))
     members = sorted(ctx.guild.members, key=lambda m: m.joined_at)
-    embed.add_field(name="Join position", value=str(members.index(user)+1))
-    embed.add_field(name="Registered", value=user.created_at.strftime(date_format))
+    embed.add_field(name="Join position", value=str(members.index(member)+1))
+    embed.add_field(name="Registered", value=member.created_at.strftime(date_format))
     if len(user.roles) > 1:
-        role_string = ' '.join([r.mention for r in user.roles][1:])
-        embed.add_field(name="Roles [{}]".format(len(user.roles)-1), value=role_string, inline=False)
-    perm_string = ', '.join([str(p[0]).replace("_", " ").title() for p in user.guild_permissions if p[1]])
+        role_string = ' '.join([r.mention for r in member.roles][1:])
+        embed.add_field(name="Roles [{}]".format(len(member.roles)-1), value=role_string, inline=False)
+    perm_string = ', '.join([str(p[0]).replace("_", " ").title() for p in member.guild_permissions if p[1]])
     embed.add_field(name="Guild permissions", value=perm_string, inline=False)
-    embed.set_footer(text='ID: ' + str(user.id))
+    embed.set_footer(text='ID: ' + str(member.id))
     await ctx.send(embed=embed)
             
 bot.run("ODI2MjI4NzU2NjU3MDc4Mjcy.YGJbfg.r_h2j1FQ4XZAsV3ptNnux7eMtGQ")
