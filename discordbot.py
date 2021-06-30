92590dfc37b5ce61c2ea3269c7b8300530ddd6d9
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-06-30 17:45:04 +0900

    detail書き直し？試作段階

--- discordbot.py
+++ discordbot.py
@@ -501,6 +501,98 @@ async def detail(ctx, command = "コマンド名"):
         await ctx.reply(embed=embed)
 
 @bot.command()
+async def detailsisaku(ctx, type=None):
+    async with ctx.typing():
+        await asyncio.sleep(0)
+    dhelp = discord.Embed(title="DETAIL-help", description="困ったときはを表示します。", colour=0x3498db)
+    dhelp.set_image(url="https://media.discordapp.net/attachments/859408401419599882/859409365140635688/unknown.png")
+    dlist = discord.Embed(title="DETAIL-list", description="コマンドリストを表示します。", colour=0x3498db)
+    dlist.set_footer(text="リアクションページを使用できます。")
+    dlist.set_image(url="https://media.discordapp.net/attachments/859408401419599882/859409537252327434/unknown.png")
+    dprof = discord.Embed(title="DETAIL-prof", description="CuBOTのプロフィールを表示します。", colour=0x3498db)
+    dprof.set_image(url="https://media.discordapp.net/attachments/826804140398215218/829292378241105950/unknown.png")
+    dping = discord.Embed(title="DETAIL-ping", description="CuBOTのping値を表示します。", colour=0x3498db)
+    dping.set_image(url="https://media.discordapp.net/attachments/826804140398215218/829292685457621032/unknown.png")
+    dkick = discord.Embed(title="DETAIL-kick", description="ユーザーをキックします。", colour=0x3498db)
+    dkick.add_field(name="使い方", value="Cu!kick [ユーザー名]", inline=True)
+    dkick.set_footer(text="このコマンドを実行できるのは管理者のみです。")
+    dkick.set_image(url="https://media.discordapp.net/attachments/826804140398215218/829293398682763284/unknown.png")
+    dban = discord.Embed(title="DETAIL-ban", description="ユーザーをBANします。", colour=0x3498db)
+    dban.add_field(name="使い方", value="Cu!ban [ユーザー名]", inline=True)
+    dban.set_footer(text="このコマンドを実行できるのは管理者のみです。")
+    dban.set_image(url="https://images-ext-2.discordapp.net/external/9S1B_5tzfHj-E7W1P92sT9uoMJgLyCIPoKUEWM2J338/https/media.discordapp.net/attachments/826804140398215218/829293782284894258/unknown.png")
+    dunban = discord.Embed(title="DETAIL-unban", description="ユーザーのBANを解除します。", colour=0x3498db)
+    dunban.add_field(name="使い方", value="Cu!unban [ユーザーID]", inline=True)
+    dunban.set_footer(text="このコマンドを実行できるのは管理者のみです。")
+    dunban.set_image(url="https://media.discordapp.net/attachments/826803343669854229/859407084339986452/unknown.png")
+    dtimer = discord.Embed(title="DETAIL-timer", description="タイマーをセットします。", colour=0x3498db)
+    dtimer.add_field(name="使い方", value="Cu!timer [秒数]", inline=True)
+    dtimer.set_image(url="https://media.discordapp.net/attachments/826804140398215218/829292950793879552/unknown.png")
+    dpoll = discord.Embed(title="DETAIL-poll", description="投票パネルを作成します。", colour=0x3498db)
+    dpoll.add_field(name="使い方", value="Cu!poll [議題] [項目1] [項目2] [項目3] [項目4]", inline=True)
+    dpoll.set_footer(text="選択肢は4つまで作成できます。")
+    dpoll.set_image(url="https://media.discordapp.net/attachments/826804140398215218/829293852077588500/unknown.png")
+    drect = discord.Embed(title="DETAIL-rect", description="募集パネルを作成します。", colour=0x3498db)
+    drect.add_field(name="使い方", value="Cu!rect [募集内容] [募集人数] [締め切り時間]", inline=True)
+    drect.set_image(url="https://media.discordapp.net/attachments/826804140398215218/829293919971967016/unknown.png")
+    dembed = discord.Embed(title="DETAIL-embed", description="Embedパネルを作成します。", colour=0x3498db)
+    dembed.add_field(name="使い方", value="Cu!embed [タイトル] [説明]", inline=True)
+    dembed.set_image(url="https://media.discordapp.net/attachments/826804140398215218/829294113576452096/unknown.png")
+    dcalcu = discord.Embed(title="DETAIL-calcu", description="計算をします。", colour=0x3498db)
+    dcalcu.add_field(name="使い方", value="Cu!calcu [数値1] [算法] [数値2]", inline=True)
+    dcalcu.set_image(url="https://media.discordapp.net/attachments/826804140398215218/844209477657559060/unknown.png")
+    dtime = discord.Embed(title="DETAIL-time", description="現在時刻を表示します。", colour=0x3498db)
+    dtime.set_image(url="https://media.discordapp.net/attachments/826804140398215218/829294591185256518/unknown.png")
+    ddetail = discord.Embed(title="DETAIL-detail", description="各コマンドの詳細を表示します。", colour=0x3498db)
+    ddetail.add_field(name="使い方", value="Cu!detail [コマンド名]", inline=True)
+    ddetail.set_image(url="https://media.discordapp.net/attachments/826804140398215218/829295373410631721/unknown.png")
+    dinvite = discord.Embed(title="DETAIL-invite", description="招待リンクの総使用数を算出します。", colour=0x3498db)
+    dinvite.add_field(name="使い方", value="Cu!invite [ユーザー名]", inline=True)
+    dinvite.set_image(url="https://media.discordapp.net/attachments/826804140398215218/844209266934939680/unknown.png")
+    dfortune = discord.Embed(title="DETAIL-fortune", description="おみくじが引けます。", colour=0x3498db)
+    dfortune.set_image(url="https://media.discordapp.net/attachments/826804140398215218/829296454110674954/unknown.png")
+    drps = discord.Embed(title="DETAIL-rps", description="じゃんけんができます。", colour=0x3498db)
+    drps.set_image(url="https://media.discordapp.net/attachments/826804140398215218/829296691290308618/unknown.png")
+    ddice = discord.Embed(title="DETAIL-dice", description="サイコロを振れます。", colour=0x3498db)
+    ddice.set_image(url="https://media.discordapp.net/attachments/826804140398215218/829296842063347742/unknown.png")
+    dpun = discord.Embed(title="DETAIL-pun", description="ダジャレが聞けます。", colour=0x3498db)
+    dpun.set_image(url="https://media.discordapp.net/attachments/826804140398215218/829297151213043722/unknown.png")
+    dcquiz = discord.Embed(title="DETAIL-cquiz", description="暗算クイズができます。", colour=0x3498db)
+    dcquiz.set_image(url="https://media.discordapp.net/attachments/826804140398215218/829297392356556820/unknown.png")
+    dcoin = discord.Embed(title="DETAIL-coin", description="コイントスができます。", colour=0x3498db)
+    dcoin.set_image(url="https://media.discordapp.net/attachments/826804140398215218/830784293148033042/unknown.png")
+    dslot = discord.Embed(title="DETAIL-slot", description="スロットができます。", colour=0x3498db)
+    dslot.set_image(url="https://media.discordapp.net/attachments/826804140398215218/832000993205682206/unknown.png")
+    dtotusi = discord.Embed(title="DETAIL-totusi", description="突然の死AAを作成します。", colour=0x3498db)
+    dtotusi.add_field(name="使い方", value="Cu!totusi [message]", inline=True)
+    dtotusi.set_image(url="https://media.discordapp.net/attachments/826804140398215218/838268795982053406/unknown.png")
+    dtotusi.set_footer(text="半角テキスト、絵文字、空白等は対応していません。")
+    pages = [dhelp, dlist, dprof, dping, dkick, dban, dunban, dtimer, dpoll, drect, dembed, dcalcu, dtime, ddetail, dinvite, dfortune, drps, ddice, dpun, dcquiz, dcoin, dslot, dtotusi]
+    page = 0
+    message = await ctx.reply(embed=pages[page])
+    await message.add_reaction("◀️")
+    await message.add_reaction("▶️")
+    def check(reaction, user):
+        return user == ctx.author and str(reaction.emoji) in ["◀️", "▶️"]
+    while True:
+        try:
+            reaction, user = await bot.wait_for("reaction_add", timeout=60, check=check)
+            if str(reaction.emoji) == "▶️" and page != 4:
+                page += 1
+                await message.edit(embed=pages[page])
+                await message.remove_reaction(reaction, user)
+            elif str(reaction.emoji) == "◀️" and page > 0:
+                page -= 1
+                await message.edit(embed=pages[page])
+                await message.remove_reaction(reaction, user)
+            else:
+                await message.remove_reaction(reaction, user)
+        except asyncio.TimeoutError:
+            await message.edit(embed=embed)
+            await message.clear_reactions()
+            break
+       
+@bot.command()
 async def coin(ctx):
     async with ctx.typing():
         await asyncio.sleep(0)
