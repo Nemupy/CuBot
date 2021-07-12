1c60976866615c5584b73639d4c78e747bfbf85a
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-07-12 19:53:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -20,7 +20,7 @@ async def on_ready():
     members = 0
     for guild in bot.guilds:
         members += guild.member_count - 1
-    await bot.change_presence(activity=discord.Activity(name=f"Cu!help | {str(servers)}servers | {str(members)}users", type=3))                                                                                           
+    await bot.change_presence(activity=discord.Activity(name=f"ただいまメンテナンス中！ | {str(servers)}servers | {str(members)}users", type=3))                                                                                           
                                                                  
 @bot.event
 async def on_command_error(ctx, error):
