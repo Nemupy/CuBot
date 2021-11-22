1dea49f7794ac7207c9ac8ef028e6a2f5ca9b067
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-11-22 17:57:36 +0900

    再起動のやーつ

--- Cogs/admin.py
+++ Cogs/admin.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import os
+import sys
 
 class AppCmdAdmin(commands.Cog):
     def __init__(self, bot):
@@ -7,10 +9,10 @@ class AppCmdAdmin(commands.Cog):
         self._last_member = None
         
     @commands.command()
-    async def restart(self,ctx):
+    async def restart(self, ctx):
         if ctx.author.id == 798439010594717737:
             await ctx.reply("再起動を実行中です・・・")
-            restart_self.bot()
+            os.execv(sys.executable, ['python'] + sys.argv)
             
 def setup(bot):
     return bot.add_cog(AppCmdAdmin(bot))
