e079094e283628ee6d4caac4860470e8421c776a
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-11-22 17:52:24 +0900

    extension

--- discordbot.py
+++ discordbot.py
@@ -33,5 +33,6 @@ bot.load_extension("Cogs.bot")
 bot.load_extension("Cogs.tool")
 bot.load_extension("Cogs.data")
 bot.load_extension("Cogs.variety")
+bot.load_extension("Cogs.admin")
 
 bot.run(token)
