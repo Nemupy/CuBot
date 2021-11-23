c82b86fd73844716bc30c85827d74c712ae56c9a
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-11-23 21:06:55 +0900

    消した

--- discordbot.py
+++ discordbot.py
@@ -12,21 +12,10 @@ bot = commands.Bot(
     case_insensitive=True
 )
 
-
 token = os.environ["token"]
 
 def restart_bot():
   os.execv(sys.executable, ['python'] + sys.argv)
-
-@bot.event
-async def on_ready():
-    servers = len(bot.guilds)
-    members = 0
-    for guild in bot.guilds:
-        members += guild.member_count - 1
-    await bot.change_presence(
-        activity=discord.Activity(name=f"Cu!help | {str(servers)}servers | {str(members)}users", type=3)
-    )
 
 bot.load_extension("Cogs.event")
 bot.load_extension("Cogs.bot")
