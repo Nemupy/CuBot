0b78cbe751ae2dbfa21a8ab4cd93eeab44cddccb
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-12-20 23:22:04 +0900

    change :リストの更新

--- Cogs/bot.py
+++ Cogs/bot.py
@@ -170,7 +170,7 @@ class AppCmdBot(commands.Cog):
                 name=":video_game: 》バラエティ", value="`fortune` `rps` `dice` `pun` `cquiz` `coin` `slot` `totusi` `5000` `neko`",
                 inline=False
             )
-            embed1.add_field(name=":game_die: 》アクティビティ", value="`youtube` `betrayal` `fishington`", inline=False)
+            embed1.add_field(name=":game_die: 》アクティビティ", value="`youtube` `betrayal` `fishington` `poker` `chess`", inline=False)
             embed2 = discord.Embed(title="ヘルプ-BOTの招待方法",
                                    description="①[招待リンク](https://discord.com/api/oauth2/authorize?client_id=826228756657078272&permissions=8&scope=bot)を開きます。\n②追加したいサーバーを選びます。\n③付与したい権限を選びます。\n　※権限が足りないとエラーを吐きます。\n　　管理者権限付与がオススメ！\n④必要に応じて認証を済ませます。\n⑤招待完了！",
                                    colour=0x3498DB)
@@ -241,7 +241,7 @@ class AppCmdBot(commands.Cog):
             name=":video_game: 》バラエティ", value="`fortune` `rps` `dice` `pun` `cquiz` `coin` `slot` `totusi` `5000` `neko`",
             inline=False
         )
-        embed.add_field(name=":game_die: 》アクティビティ", value="`youtube` `betrayal` `fishington`", inline=False)
+        embed.add_field(name=":game_die: 》アクティビティ", value="`youtube` `betrayal` `fishington` `poker` `chess`", inline=False)
         embed1 = discord.Embed(title="コマンドリスト-BOT", description="使用可能なコマンド一覧です♪\n各コマンドの詳細は`Cu!help [コマンド名]`で確認できます♪", colour=0x3498DB)
         embed1.add_field(
             name=":robot: 》BOT",
@@ -282,7 +282,9 @@ class AppCmdBot(commands.Cog):
             name=":game_die: 》アクティビティ",
             value="`youtube`：YouTubeTogetherを開始します。\n"
                   "`betrayal`：Betrayal.ioを開始します。\n"
-                  "`fishington`：Fishington.ioを開始します。",
+                  "`fishington`：Fishington.ioを開始します。\n"
+                  "`poker`：PokerNightを開始します。\n"
+                  "`chess`：ChessInTheParkを開始します。",
         )
         pages = [embed, embed1, embed2, embed3, embed4, embed5]
         page = 0
