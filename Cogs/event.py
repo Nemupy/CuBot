c7a9a0b8df29ed86267ad2bb1bcde4f5bd92bb2f
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-12-25 21:36:31 +0900

    change : botのステータスの更新方法を変えた

--- Cogs/event.py
+++ Cogs/event.py
@@ -19,13 +19,6 @@ class AppCmdEvent(commands.Cog):
 
     @commands.Cog.listener()
     async def on_guild_join(self,guild):
-        member_count = guild.member_count
-        embed = discord.Embed(title="導入してくれてありがとう！",
-                              description=f"<@826228756657078272>が導入されました。\nCuは{member_count}人目のユーザーです。",
-                              color=0x3498DB)
-        embed.set_thumbnail(
-            url="https://images-ext-1.discordapp.net/external/bi88_iGaiR-z5Oc6L0OBqkgDkY1UMe7sIPX94aZu8RE/%3Fformat%3Djpg%26name%3Dlarge/https/pbs.twimg.com/media/EfWoupuUYAAwuTv?width=473&height=473")
-        await guild.system_channel.send(embed=embed)
         servers = len(self.bot.guilds)
         members = 0
         for guild in self.bot.guilds:
@@ -33,6 +26,13 @@ class AppCmdEvent(commands.Cog):
         await self.bot.change_presence(
             activity=discord.Activity(name=f"Cu!help | {str(servers)}servers | {str(members)}users", type=3)
         )
+        member_count = guild.member_count
+        embed = discord.Embed(title="導入してくれてありがとう！",
+                              description=f"<@826228756657078272>が導入されました。\nCuは{member_count}人目のユーザーです。",
+                              color=0x3498DB)
+        embed.set_thumbnail(
+            url="https://images-ext-1.discordapp.net/external/bi88_iGaiR-z5Oc6L0OBqkgDkY1UMe7sIPX94aZu8RE/%3Fformat%3Djpg%26name%3Dlarge/https/pbs.twimg.com/media/EfWoupuUYAAwuTv?width=473&height=473")
+        await guild.system_channel.send(embed=embed)
         
     @commands.Cog.listener()
     async def on_guild_remove(self,guild):
@@ -81,6 +81,13 @@ class AppCmdEvent(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self,member):
+        servers = len(self.bot.guilds)
+        members = 0
+        for guild in self.bot.guilds:
+            members += guild.member_count - 1
+        await self.bot.change_presence(
+            activity=discord.Activity(name=f"Cu!help | {str(servers)}servers | {str(members)}users", type=3)
+        )
         if member.guild.system_channel:
             guild = member.guild
             guild_name = member.guild.name
@@ -95,6 +102,13 @@ class AppCmdEvent(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_remove(self,member):
+        servers = len(self.bot.guilds)
+        members = 0
+        for guild in self.bot.guilds:
+            members += guild.member_count - 1
+        await self.bot.change_presence(
+            activity=discord.Activity(name=f"Cu!help | {str(servers)}servers | {str(members)}users", type=3)
+        )
         if member.guild.system_channel:
             embed = discord.Embed(title="また来てね！", description=f"{member.mention}さんが退室しました。", colour=0x3498DB)
             embed.set_thumbnail(url=member.avatar.url)
