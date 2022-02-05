2025fb4bd4d5ff2a598c17be5acd65be0185a57b
音夢?! <82650472+Nemu627@users.noreply.github.com>
2022-02-05 10:33:23 +0900

    clean : 整理

--- Cogs/data.py
+++ Cogs/data.py
@@ -4,13 +4,14 @@ import asyncio
 import datetime
 import pytz
 
+
 class AppCmdData(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
 
     @commands.command()
-    async def time(self,ctx,zone=None):
+    async def time(self, ctx, zone=None):
         async with ctx.typing():
             await asyncio.sleep(0)
         if zone == None:
@@ -35,40 +36,46 @@ class AppCmdData(commands.Cog):
         else:
             now = datetime.datetime.now(pytz.timezone(zone))
             time = now.strftime("%m月%d日 %H:%M")
-            embed = discord.Embed(title="現在の時刻",description=f"{zone}：{time}",colour=0x3498DB)
+            embed = discord.Embed(
+                title="現在の時刻", description=f"{zone}：{time}", colour=0x3498DB)
             await ctx.send(embed=embed)
-            
+
     @commands.command()
-    async def detail(self,ctx, command="コマンド名"):
+    async def detail(self, ctx, command="コマンド名"):
         async with ctx.typing():
             await asyncio.sleep(0)
         if command == "help":
-            embed = discord.Embed(title="DETAIL-help", description="困ったときはを表示します。", colour=0x3498DB)
+            embed = discord.Embed(title="DETAIL-help",
+                                  description="困ったときはを表示します。", colour=0x3498DB)
             embed.set_image(
                 url="https://media.discordapp.net/attachments/859408401419599882/859409365140635688/unknown.png"
             )
             await ctx.reply(embed=embed)
         elif command == "list":
-            embed = discord.Embed(title="DETAIL-list", description="コマンドリストを表示します。", colour=0x3498DB)
+            embed = discord.Embed(
+                title="DETAIL-list", description="コマンドリストを表示します。", colour=0x3498DB)
             embed.set_footer(text="リアクションページを使用できます。")
             embed.set_image(
                 url="https://media.discordapp.net/attachments/859408401419599882/859409537252327434/unknown.png"
             )
             await ctx.reply(embed=embed)
         elif command == "prof":
-            embed = discord.Embed(title="DETAIL-prof", description="CuBOTのプロフィールを表示します。", colour=0x3498DB)
+            embed = discord.Embed(
+                title="DETAIL-prof", description="CuBOTのプロフィールを表示します。", colour=0x3498DB)
             embed.set_image(
                 url="https://media.discordapp.net/attachments/826804140398215218/829292378241105950/unknown.png"
             )
             await ctx.reply(embed=embed)
         elif command == "ping":
-            embed = discord.Embed(title="DETAIL-ping", description="CuBOTのping値を表示します。", colour=0x3498DB)
+            embed = discord.Embed(
+                title="DETAIL-ping", description="CuBOTのping値を表示します。", colour=0x3498DB)
             embed.set_image(
                 url="https://media.discordapp.net/attachments/826804140398215218/829292685457621032/unknown.png"
             )
             await ctx.reply(embed=embed)
         elif command == "kick":
-            embed = discord.Embed(title="DETAIL-kick", description="ユーザーをキックします。", colour=0x3498DB)
+            embed = discord.Embed(title="DETAIL-kick",
+                                  description="ユーザーをキックします。", colour=0x3498DB)
             embed.add_field(name="使い方", value="Cu!kick [ユーザー名]", inline=True)
             embed.set_footer(text="このコマンドを実行できるのは管理者のみです。")
             embed.set_image(
@@ -76,7 +83,8 @@ class AppCmdData(commands.Cog):
             )
             await ctx.reply(embed=embed)
         elif command == "ban":
-            embed = discord.Embed(title="DETAIL-ban", description="ユーザーをBANします。", colour=0x3498DB)
+            embed = discord.Embed(title="DETAIL-ban",
+                                  description="ユーザーをBANします。", colour=0x3498DB)
             embed.add_field(name="使い方", value="Cu!ban [ユーザー名]", inline=True)
             embed.set_footer(text="このコマンドを実行できるのは管理者のみです。")
             embed.set_image(
@@ -85,7 +93,8 @@ class AppCmdData(commands.Cog):
             )
             await ctx.reply(embed=embed)
         elif command == "unban":
-            embed = discord.Embed(title="DETAIL-unban", description="ユーザーのBANを解除します。", colour=0x3498DB)
+            embed = discord.Embed(
+                title="DETAIL-unban", description="ユーザーのBANを解除します。", colour=0x3498DB)
             embed.add_field(name="使い方", value="Cu!unban [ユーザーID]", inline=True)
             embed.set_footer(text="このコマンドを実行できるのは管理者のみです。")
             embed.set_image(
@@ -93,106 +102,125 @@ class AppCmdData(commands.Cog):
             )
             await ctx.reply(embed=embed)
         elif command == "timer":
-            embed = discord.Embed(title="DETAIL-timer", description="タイマーをセットします。", colour=0x3498DB)
+            embed = discord.Embed(title="DETAIL-timer",
+                                  description="タイマーをセットします。", colour=0x3498DB)
             embed.add_field(name="使い方", value="Cu!timer [秒数]", inline=True)
             embed.set_image(
                 url="https://media.discordapp.net/attachments/826804140398215218/829292950793879552/unknown.png"
             )
             await ctx.reply(embed=embed)
         elif command == "poll":
-            embed = discord.Embed(title="DETAIL-poll", description="投票パネルを作成します。", colour=0x3498DB)
-            embed.add_field(name="使い方", value="Cu!poll [議題] [項目1] [項目2] [項目3] [項目4]", inline=True)
+            embed = discord.Embed(title="DETAIL-poll",
+                                  description="投票パネルを作成します。", colour=0x3498DB)
+            embed.add_field(
+                name="使い方", value="Cu!poll [議題] [項目1] [項目2] [項目3] [項目4]", inline=True)
             embed.set_footer(text="選択肢は4つまで作成できます。")
             embed.set_image(
                 url="https://media.discordapp.net/attachments/826804140398215218/829293852077588500/unknown.png"
             )
             await ctx.reply(embed=embed)
         elif command == "rect":
-            embed = discord.Embed(title="DETAIL-rect", description="募集パネルを作成します。", colour=0x3498DB)
-            embed.add_field(name="使い方", value="Cu!rect [募集内容] [募集人数] [締め切り時間]", inline=True)
+            embed = discord.Embed(title="DETAIL-rect",
+                                  description="募集パネルを作成します。", colour=0x3498DB)
+            embed.add_field(
+                name="使い方", value="Cu!rect [募集内容] [募集人数] [締め切り時間]", inline=True)
             embed.set_image(
                 url="https://media.discordapp.net/attachments/826804140398215218/829293919971967016/unknown.png"
             )
             await ctx.reply(embed=embed)
         elif command == "embed":
-            embed = discord.Embed(title="DETAIL-embed", description="Embedパネルを作成します。", colour=0x3498DB)
-            embed.add_field(name="使い方", value="Cu!embed [タイトル] [説明]", inline=True)
+            embed = discord.Embed(
+                title="DETAIL-embed", description="Embedパネルを作成します。", colour=0x3498DB)
+            embed.add_field(
+                name="使い方", value="Cu!embed [タイトル] [説明]", inline=True)
             embed.set_image(
                 url="https://media.discordapp.net/attachments/826804140398215218/829294113576452096/unknown.png"
             )
             await ctx.reply(embed=embed)
         elif command == "calcu":
-            embed = discord.Embed(title="DETAIL-calcu", description="計算をします。", colour=0x3498DB)
-            embed.add_field(name="使い方", value="Cu!calcu [数値1] [算法] [数値2]", inline=True)
+            embed = discord.Embed(title="DETAIL-calcu",
+                                  description="計算をします。", colour=0x3498DB)
+            embed.add_field(
+                name="使い方", value="Cu!calcu [数値1] [算法] [数値2]", inline=True)
             embed.set_image(
                 url="https://media.discordapp.net/attachments/826804140398215218/844209477657559060/unknown.png"
             )
             await ctx.reply(embed=embed)
         elif command == "time":
-            embed = discord.Embed(title="DETAIL-time", description="現在時刻を表示します。", colour=0x3498DB)
+            embed = discord.Embed(title="DETAIL-time",
+                                  description="現在時刻を表示します。", colour=0x3498DB)
             embed.set_image(
                 url="https://media.discordapp.net/attachments/826804140398215218/829294591185256518/unknown.png"
             )
             await ctx.reply(embed=embed)
         elif command == "detail":
-            embed = discord.Embed(title="DETAIL-detail", description="各コマンドの詳細を表示します。", colour=0x3498DB)
+            embed = discord.Embed(
+                title="DETAIL-detail", description="各コマンドの詳細を表示します。", colour=0x3498DB)
             embed.add_field(name="使い方", value="Cu!detail [コマンド名]", inline=True)
             embed.set_image(
                 url="https://media.discordapp.net/attachments/826804140398215218/829295373410631721/unknown.png"
             )
             await ctx.reply(embed=embed)
         elif command == "invite":
-            embed = discord.Embed(title="DETAIL-invite", description="招待リンクの総使用数を算出します。", colour=0x3498DB)
+            embed = discord.Embed(
+                title="DETAIL-invite", description="招待リンクの総使用数を算出します。", colour=0x3498DB)
             embed.add_field(name="使い方", value="Cu!invite [ユーザー名]", inline=True)
             embed.set_image(
                 url="https://media.discordapp.net/attachments/826804140398215218/844209266934939680/unknown.png"
             )
             await ctx.reply(embed=embed)
         elif command == "fortune":
-            embed = discord.Embed(title="DETAIL-fortune", description="おみくじが引けます。", colour=0x3498DB)
+            embed = discord.Embed(title="DETAIL-fortune",
+                                  description="おみくじが引けます。", colour=0x3498DB)
             embed.set_image(
                 url="https://media.discordapp.net/attachments/826804140398215218/829296454110674954/unknown.png"
             )
             await ctx.reply(embed=embed)
         elif command == "rps":
-            embed = discord.Embed(title="DETAIL-rps", description="じゃんけんができます。", colour=0x3498DB)
+            embed = discord.Embed(title="DETAIL-rps",
+                                  description="じゃんけんができます。", colour=0x3498DB)
             embed.set_image(
                 url="https://media.discordapp.net/attachments/826804140398215218/829296691290308618/unknown.png"
             )
             await ctx.reply(embed=embed)
         elif command == "dice":
-            embed = discord.Embed(title="DETAIL-dice", description="サイコロを振れます。", colour=0x3498DB)
+            embed = discord.Embed(title="DETAIL-dice",
+                                  description="サイコロを振れます。", colour=0x3498DB)
             embed.set_image(
                 url="https://media.discordapp.net/attachments/826804140398215218/829296842063347742/unknown.png"
             )
             await ctx.reply(embed=embed)
         elif command == "pun":
-            embed = discord.Embed(title="DETAIL-pun", description="ダジャレが聞けます。", colour=0x3498DB)
+            embed = discord.Embed(title="DETAIL-pun",
+                                  description="ダジャレが聞けます。", colour=0x3498DB)
             embed.set_image(
                 url="https://media.discordapp.net/attachments/826804140398215218/829297151213043722/unknown.png"
             )
             await ctx.reply(embed=embed)
         elif command == "cquiz":
-            embed = discord.Embed(title="DETAIL-cquiz", description="暗算クイズができます。", colour=0x3498DB)
+            embed = discord.Embed(title="DETAIL-cquiz",
+                                  description="暗算クイズができます。", colour=0x3498DB)
             embed.set_image(
                 url="https://media.discordapp.net/attachments/826804140398215218/829297392356556820/unknown.png"
             )
             await ctx.reply(embed=embed)
         elif command == "coin":
-            embed = discord.Embed(title="DETAIL-coin", description="コイントスができます。", colour=0x3498DB)
+            embed = discord.Embed(title="DETAIL-coin",
+                                  description="コイントスができます。", colour=0x3498DB)
             embed.set_image(
                 url="https://media.discordapp.net/attachments/826804140398215218/830784293148033042/unknown.png"
             )
             await ctx.reply(embed=embed)
         elif command == "slot":
-            embed = discord.Embed(title="DETAIL-slot", description="スロットができます。", colour=0x3498DB)
+            embed = discord.Embed(title="DETAIL-slot",
+                                  description="スロットができます。", colour=0x3498DB)
             embed.set_image(
                 url="https://media.discordapp.net/attachments/826804140398215218/832000993205682206/unknown.png"
             )
             await ctx.reply(embed=embed)
 
     @commands.command()
-    async def invite(self,ctx, member: discord.Member = None):
+    async def invite(self, ctx, member: discord.Member = None):
         async with ctx.typing():
             await asyncio.sleep(0)
         if member is None:
@@ -207,6 +235,7 @@ class AppCmdData(commands.Cog):
             title="招待リンクの使用数", description=f"{user.mention}さんは**{total_invites}人**のメンバーを招待しました！", color=0x3498DB
         )
         await ctx.reply(embed=embed)
+
 
 def setup(bot):
     return bot.add_cog(AppCmdData(bot))
--- Cogs/event.py
+++ Cogs/event.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import traceback
 
+
 class AppCmdEvent(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -14,17 +15,20 @@ class AppCmdEvent(commands.Cog):
         for guild in self.bot.guilds:
             members += guild.member_count - 1
         await self.bot.change_presence(
-            activity=discord.Activity(name=f"Cu!help | {str(servers)}servers | {str(members)}users", type=3)
+            activity=discord.Activity(
+                name=f"Cu!help | {str(servers)}servers | {str(members)}users", type=3)
         )
 
     @commands.Cog.listener()
-    async def on_command_error(self,ctx, error):
+    async def on_command_error(self, ctx, error):
         orig_error = getattr(error, "original", error)
-        error_msg = "".join(traceback.TracebackException.from_exception(orig_error).format())
+        error_msg = "".join(
+            traceback.TracebackException.from_exception(orig_error).format())
         if isinstance(error, commands.errors.MissingPermissions):
             embed = discord.Embed(
                 title="エラー-権限不足",
-                description="権限が不足しています。権限設定をご確認ください。\nこのエラーが多発する場合は[公式サーバー](https://discord.gg/RFPQmRnv2j)までお問い合わせください。\n```" + error_msg + "```",
+                description="権限が不足しています。権限設定をご確認ください。\nこのエラーが多発する場合は[公式サーバー](https://discord.gg/RFPQmRnv2j)までお問い合わせください。\n```" +
+                error_msg + "```",
                 colour=0x3498DB,
             )
             await ctx.reply(embed=embed)
@@ -32,7 +36,8 @@ class AppCmdEvent(commands.Cog):
             if isinstance(error, commands.errors.CommandNotFound):
                 embed = discord.Embed(
                     title="エラー-不明なコマンド",
-                    description="不明なコマンドです。`Cu!list`でコマンドを確認してください。\nこのエラーが多発する場合は[公式サーバー](https://discord.gg/RFPQmRnv2j)までお問い合わせください。\n```" + error_msg + "```",
+                    description="不明なコマンドです。`Cu!list`でコマンドを確認してください。\nこのエラーが多発する場合は[公式サーバー](https://discord.gg/RFPQmRnv2j)までお問い合わせください。\n```" +
+                    error_msg + "```",
                     colour=0x3498DB,
                 )
                 await ctx.reply(embed=embed)
@@ -40,26 +45,29 @@ class AppCmdEvent(commands.Cog):
                 if isinstance(error, commands.errors.MissingRequiredArgument):
                     embed = discord.Embed(
                         title="エラー-引数不足",
-                        description="引数が不足しています。`Cu!help [コマンド名]`でコマンドを確認してください。\nこのエラーが多発する場合は[公式サーバー](https://discord.gg/RFPQmRnv2j)までお問い合わせください。\n```" + error_msg + "```",
+                        description="引数が不足しています。`Cu!help [コマンド名]`でコマンドを確認してください。\nこのエラーが多発する場合は[公式サーバー](https://discord.gg/RFPQmRnv2j)までお問い合わせください。\n```" +
+                        error_msg + "```",
                         colour=0x3498DB,
                     )
                     await ctx.reply(embed=embed)
                 else:
                     embed = discord.Embed(
                         title="エラー",
-                        description="予期せぬエラーが発生しました。\nこのエラーが多発する場合は[公式サーバー](https://discord.gg/RFPQmRnv2j)までお問い合わせください。\n```" + error_msg + "```",
+                        description="予期せぬエラーが発生しました。\nこのエラーが多発する場合は[公式サーバー](https://discord.gg/RFPQmRnv2j)までお問い合わせください。\n```" +
+                        error_msg + "```",
                         colour=0x3498DB,
                     )
                     await ctx.send(embed=embed)
-                    
+
     @commands.Cog.listener()
-    async def on_guild_join(self,guild):
+    async def on_guild_join(self, guild):
         servers = len(self.bot.guilds)
         members = 0
         for guild in self.bot.guilds:
             members += guild.member_count - 1
         await self.bot.change_presence(
-            activity=discord.Activity(name=f"Cu!help | {str(servers)}servers | {str(members)}users", type=3)
+            activity=discord.Activity(
+                name=f"Cu!help | {str(servers)}servers | {str(members)}users", type=3)
         )
         member_count = guild.member_count
         embed = discord.Embed(title="導入してくれてありがとう！",
@@ -68,19 +76,20 @@ class AppCmdEvent(commands.Cog):
         embed.set_thumbnail(
             url="https://images-ext-1.discordapp.net/external/bi88_iGaiR-z5Oc6L0OBqkgDkY1UMe7sIPX94aZu8RE/%3Fformat%3Djpg%26name%3Dlarge/https/pbs.twimg.com/media/EfWoupuUYAAwuTv?width=473&height=473")
         await guild.system_channel.send(embed=embed)
-        
+
     @commands.Cog.listener()
-    async def on_guild_remove(self,guild):
+    async def on_guild_remove(self, guild):
         servers = len(self.bot.guilds)
         members = 0
         for guild in self.bot.guilds:
             members += guild.member_count - 1
         await self.bot.change_presence(
-            activity=discord.Activity(name=f"Cu!help | {str(servers)}servers | {str(members)}users", type=3)
+            activity=discord.Activity(
+                name=f"Cu!help | {str(servers)}servers | {str(members)}users", type=3)
         )
 
     @commands.Cog.listener()
-    async def on_member_join(self,member):
+    async def on_member_join(self, member):
         if member.guild.system_channel:
             guild = member.guild
             guild_name = member.guild.name
@@ -94,14 +103,15 @@ class AppCmdEvent(commands.Cog):
             await member.guild.system_channel.send(embed=embed)
 
     @commands.Cog.listener()
-    async def on_member_remove(self,member):
+    async def on_member_remove(self, member):
         if member.guild.system_channel:
-            embed = discord.Embed(title="また来てね！", description=f"{member.mention}さんが退室しました。", colour=0x3498DB)
+            embed = discord.Embed(
+                title="また来てね！", description=f"{member.mention}さんが退室しました。", colour=0x3498DB)
             embed.set_thumbnail(url=member.avatar.url)
             await member.guild.system_channel.send(embed=embed)
 
     @commands.Cog.listener()
-    async def on_message(self,message):
+    async def on_message(self, message):
         if message.author.bot:
             return
         # elif message.type == discord.MessageType.new_member:
@@ -109,6 +119,7 @@ class AppCmdEvent(commands.Cog):
         # return
         elif self.bot.user.id in message.raw_mentions:
             await message.reply("お呼びでしょうか！お困りの際は`Cu!help`と送信してみて下さいね♪", mention_author=False)
+
 
 def setup(bot):
     return bot.add_cog(AppCmdEvent(bot))
