6169b0e76e942dbbca1bc51e190163207992f8b6
音夢?! <82650472+Nemu627@users.noreply.github.com>
2022-02-09 22:40:18 +0900

    remove : 無駄な変数を削除

--- discordbot.py
+++ discordbot.py
@@ -1,24 +1,17 @@
 import discord
 from discord.ext import commands
 import os
-import sys
 
-intents = discord.Intents.all()
-client = discord.Client(intents=intents)
 bot = commands.Bot(
     command_prefix=["Cu!", "cu!"],
     help_command=None,
-    intents=intents,
+    intents=discord.Intents.all(),
     allowed_mentions=discord.AllowedMentions(
         replied_user=False, everyone=False, roles=False),
     case_insensitive=True
 )
 
 token = os.environ["token"]
-
-
-def restart_bot():
-    os.execv(sys.executable, ['python'] + sys.argv)
 
 
 bot.load_extension('jishaku')
